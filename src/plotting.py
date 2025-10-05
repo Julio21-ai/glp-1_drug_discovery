2ba10ad74ecca5723498f20a21e6e91f695bd7a2
JulioBaltazar <86627705+Julio21-ai@users.noreply.github.com>
2025-10-04 19:44:40 -0600

    Add PCA variance and visualization plots
    Added 'grafico_varianza_pca.png' and 'pca_visualization.png' for PCA analysis visualization. Updated 'Avance 2.ipynb' and 'plotting.py' to include and reference these new plots. Minor update to 'descriptores_125.csv' data.

--- src/plotting.py
+++ src/plotting.py
@@ -16,3 +16,58 @@ def identity_heatmap(identity_matrix,
     plt.xlabel("Secuencia Index")
     plt.ylabel("Secuencia Index")
     plt.show()
+
+def cumulative_variance_plot( cumulative_variance, figsize=(8, 5)):
+
+    # Generar el gráfico
+    plt.figure(figsize=figsize)
+    plt.plot(range(1, len(cumulative_variance) + 1), cumulative_variance, marker='.', linestyle='--', color='blue')
+
+    plt.title('Varianza Acumulada Explicada por Componentes Principales', fontsize=16)
+    plt.xlabel('Número de Componentes Principales')
+    plt.ylabel('Porcentaje de Varianza Acumulada')
+    plt.grid(True)
+
+    # Añadir líneas de referencia para facilitar la decisión
+    plt.axhline(y=0.95, color='red', linestyle='--', label='95% de Varianza')
+    plt.axhline(y=0.90, color='green', linestyle='--', label='90% de Varianza')
+    plt.legend()
+
+    # Mostrar el gráfico en la salida de la celda
+    plt.show()
+
+def plot_pca_2d(pca_df, pc_x='PC1', pc_y='PC2', color_by=None, label='Values', figsize=(10, 8), 
+                title='Visualización de 2 Componentes Principales (PCA)',
+                cmap='viridis'):
+    """
+    Genera y muestra un gráfico de dispersión 2D de los Componentes Principales.
+
+    Args:
+        pca_df (pd.DataFrame): DataFrame que contiene los componentes principales (ej. PC1, PC2).
+        pc_x (str): Nombre de la columna para el eje X (por defecto, 'PC1').
+        pc_y (str): Nombre de la columna para el eje Y (por defecto, 'PC2').
+        color_by (str, optional): Nombre de la columna para colorear los puntos. 
+                                  Si es None, todos los puntos serán del mismo color.
+        title (str): Título del gráfico.
+        cmap (str): Mapa de colores a utilizar si se especifica `color_by`.
+    """
+    plt.figure(figsize=figsize)
+
+    # Si se proporciona una columna para colorear, crea un gráfico de dispersión con color
+    if color_by is not None:
+        # Asegurarse de que la columna `color_by` exista en el DataFrame
+        scatter = plt.scatter(
+                    x=pca_df[pc_x],
+                    y=pca_df[pc_y],
+                    c=color_by,
+                    cmap=cmap
+                )
+        plt.colorbar(scatter, label=label)
+    else:
+        plt.scatter(x=pca_df[pc_x], y=pca_df[pc_y], c='red' )
+    
+    plt.xlabel(f'{pc_x}')
+    plt.ylabel(f'{pc_y}')
+    plt.title(title)
+    plt.grid(True)
+    plt.show()
